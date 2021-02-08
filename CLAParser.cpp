de3df1219da9a0c82fb44fe8351221042f637ee0
Phivos Papapanagiotakis <csd3823@csd.uoc.gr>
2021-02-08 19:17:53 +0200

    Base

---
+++ CLAParser.cpp
@@ -0,0 +1,13 @@
+#include "CLArgParser.h"
+
+CLArgParser::CLArgParser(void) {
+
+}
+
+CLArgParser::~CLArgParser(void) {
+
+}
+
+void CLArgParser::Parse(const char* argv[], int argc) {
+
+}
---
+++ CLAParser.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+#include <map>
+#include <vector>
+
+class CLAParser{
+private:
+	std::map<std::string, std::string> bounded;	// map of bounded args
+	std::vector<std::string> unbounded;			// vector of inorder unbounded args
+	std::map<std::string, bool> flags;
+public:
+	CLAParser(void);
+	~CLAParser(void);
+	void Parse(const char**, int);
+};
